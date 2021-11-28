3a51b903103eb4cc64f5ef2e5fefdb2b7d460cd8
Luca <fruchtzwerg123ttgmail.com>
2021-11-28 10:21:30 +0100

    updated console messages...

--- src/main/java/eu/luftiger/syncedweather/SyncedWeather.java
+++ src/main/java/eu/luftiger/syncedweather/SyncedWeather.java
@@ -5,6 +5,7 @@ import eu.luftiger.syncedweather.scheduler.CheckUpWeatherTask;
 import eu.luftiger.syncedweather.utils.ConfigService;
 import eu.luftiger.syncedweather.utils.WeatherService;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
@@ -15,28 +16,39 @@ public final class SyncedWeather extends JavaPlugin {
 	private WeatherService weatherService;
 	private ConfigService configService;
 
+	private String consolePrefix;
+	private String consoleLogo =
+			"\n   _____                          ___          __        _   _                \n" +
+			"  / ____|                        | \\ \\        / /       | | | |               \n" +
+			" | (___  _   _ _ __   ___ ___  __| |\\ \\  /\\  / /__  __ _| |_| |__   ___ _ __  \n" +
+			"  \\___ \\| | | | '_ \\ / __/ _ \\/ _` | \\ \\/  \\/ / _ \\/ _` | __| '_ \\ / _ \\ '__| \n" +
+			"  ____) | |_| | | | | (_|  __/ (_| |  \\  /\\  /  __/ (_| | |_| | | |  __/ |    \n" +
+			" |_____/ \\__, |_| |_|\\___\\___|\\__,_|   \\/  \\/ \\___|\\__,_|\\__|_| |_|\\___|_|    \n" +
+			"          __/ |                                                               \n" +
+			"         |___/";
+
 	@Override
 	public void onEnable() {
+		logger.info(consoleLogo);
+		consolePrefix = "[" + this.getDescription().getPrefix() + "]";
 
-		logger.info("------------[SyncedWeather]------------");
-		logger.info("enabling plugin...");
+		logger.info(consolePrefix + " enabling plugin...");
 
-		logger.info("loading config...");
+		logger.info(consolePrefix + " loading config...");
 		configService = new ConfigService(this);
 		configService.createDefaults();
 
 		if(configService.getConfig().getString("API_KEY").isEmpty()){
-			logger.warning("the api key is missing!!");
-			logger.info("disabling plugin...");
-			logger.info("------------[SyncedWeather]------------");
+			logger.warning(consolePrefix + " the api key is missing!!");
+			logger.info(consolePrefix + " disabling plugin...");
 			getServer().getPluginManager().disablePlugin(this);
 			return;
 		}
 
-		logger.info("loading weather-service...");
+		logger.info(consolePrefix + " loading weather-service...");
 		weatherService = new WeatherService(this);
 
-		logger.info("starting checkup-services...");
+		logger.info(consolePrefix + " starting checkup-services...");
 		if(configService.getConfig().getBoolean("SyncWeather")){
 			new CheckUpWeatherTask(this).start();
 		}
@@ -44,7 +56,6 @@ public final class SyncedWeather extends JavaPlugin {
 			new CheckUpTimeTask(this).start();
 		}
 
-		logger.info("------------[SyncedWeather]------------");
 	}
 
 	public WeatherService getWeatherService() {
